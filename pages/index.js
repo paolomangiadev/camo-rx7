b9fd21886757db39c8fb21a25b95b4c011c8256c
Paolo Mangia <paolo.mangia.dev@gmail.com>
2021-09-19 11:05:11 +0200

    bg + gif

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,29 @@ import Image from "next/image";
 export default function IndexPage() {
   const HomeViewPort = () => (
     <div className="full-height-view">
-      <Image
-        src="https://www.datocms-assets.com/55576/1631985499-loop.gif"
-        width="100"
-        height="100"
-      />
+      <div className="wrapper">
+        <Title />
+      </div>
+      <div className="wrapper">
+        <img
+          src="https://www.datocms-assets.com/55576/1631985499-loop.gif"
+          width="300"
+          height="300"
+          className="rx7_loop_img"
+          alt=""
+        />
+      </div>
     </div>
+  );
+
+  const Title = () => (
+    <img
+      src="https://www.datocms-assets.com/55576/1631991918-camorx7title.png"
+      width="1355"
+      height="195"
+      className="rx_title"
+      alt=""
+    />
   );
 
   return (
